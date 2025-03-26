90dead40bbd4c0a81a4e6add5c1267ba7715c5ce
Adam Bazzi <adam@adambazzi.ca>
2025-03-26 13:13:39 -0400

    feat: #5 NavBar fetches weather and updates theme name

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,23 +1,13 @@
+import { useEffect, useState } from 'react';
 import { NavBarProps } from '../../types';
 import './NavBar.css';
 
 export default function NavBar({ props }: { props: NavBarProps }) {
 
-
-  async function fetchWeather(date: Date): Promise<string> {
-    const url =
-      'https://api.open-meteo.com/v1/forecast?latitude=42.293&longitude=-82.9&current=temperature_2m&hourly=temperature_2m&timezone=auto&forecast_days=1';
-    const response = await fetch(url);
-    const data = await response.json();
-    return `${data.hourly.temperature_2m[date.getHours()]}`;
-  }
-
-  function handleThemeChange(e: React.ChangeEvent<HTMLSelectElement>) {
-    props.setTheme(e.target.value)
-  }
-
   const date = new Date();
+  const [weather, setWeather] = useState('');
 
+  // MM-DD-YYY
   const currentDay = new Date(
     date.getFullYear(),
     date.getMonth(),
@@ -29,10 +19,31 @@ export default function NavBar({ props }: { props: NavBarProps }) {
     month: 'long',
   });
 
+  function handleThemeChange(e: React.ChangeEvent<HTMLSelectElement>) {
+    props.setTheme(e.target.value)
+  }
+
+  useEffect(() => {
+    async function fetchWeather(date: Date): Promise<void> {
+      const url =
+        'https://api.open-meteo.com/v1/forecast?latitude=42.293&longitude=-82.9&current=temperature_2m&hourly=temperature_2m&timezone=auto&forecast_days=1';
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        setWeather(`${data.hourly.temperature_2m[date.getHours()]}`)
+      } catch (error) {
+        throw new Error(`Error fetching data: ${error}`);
+      }
+    }
+    fetchWeather(date);
+  })
+
+
   return (
     <nav>
       <h1>Comic Dialog</h1>
       <h1>{currentDay}</h1>
+      <h1>{weather}</h1>
 
       <label htmlFor="themes">Theme</label>
       <select
