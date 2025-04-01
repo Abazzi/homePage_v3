405ce3d17bf8117a6a571cebaa999de44f417ed0
Adam Bazzi <adam@adambazzi.ca>
2025-04-01 13:01:52 -0400

    cut: removed theme call on main page
    was used for testing

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,16 @@ import NavBar from './components/NavBar/NavBar';
 
 function App() {
   const [theme, setTheme] = useState('alucard');
+  /* Link Mode represents the name of the array of links in links.json
+   * that is currently being displayed on the page. In v2 the theme and link
+   * selections were combined but in v3 you can have any combo of links and
+   * themes
+   * */
   const [linkMode, setLinkMode] = useState('default');
 
   return (
     <>
       <NavBar props={{ theme: theme, setTheme: setTheme }} />
-      <h1>{theme}</h1>
     </>
   )
 }
