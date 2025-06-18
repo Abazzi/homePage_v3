22d1d65be8666278e71bba9bb184970fe09c4024
Adam Bazzi <adam@adambazzi.ca>
2025-06-18 14:25:05 -0400

    refactor: wrapped returned 'a' tag in a 'li' tag

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -2,6 +2,6 @@ import { LinkProps } from "../../types";
 
 export default function Link({ props }: { props: LinkProps }) {
   return (
-    <a href={props.url} target='_blank'>{props.name}</a>
+    <li><a href={props.url} target='_blank'>{props.name}</a></li>
   )
 }
