6c06f7abed50e959abfe5ef51e4bcebf66d36b6d
Adam Bazzi <adam@adambazzi.ca>
2025-06-18 14:25:24 -0400

    add: created Linkboard which creates the element that holds each List of Links

---
+++ src/components/LinkBoard/LinkBoard.tsx
@@ -0,0 +1,29 @@
+import { DefaultLinkGroups, LinkBoardProps, ProgrammingLinkGroups } from "../../types";
+import ListOfLinks from "../ListOfLinks/ListOfLinks";
+import linkData from '../../assets/links.json';
+
+export default function LinkBoard({ props }: { props: LinkBoardProps }) {
+  const ProgrammingLinks: ProgrammingLinkGroups = linkData.programming;
+  const DefaultLinks: DefaultLinkGroups = linkData.default;
+
+  if (props.type == "default") {
+    return (
+      <div className='link-board'>
+        <ListOfLinks props={{ linkArray: DefaultLinks.daily }} />
+        <ListOfLinks props={{ linkArray: DefaultLinks.social }} />
+        <ListOfLinks props={{ linkArray: DefaultLinks.media }} />
+        <ListOfLinks props={{ linkArray: DefaultLinks.server }} />
+      </div>
+    )
+  } else if (props.type == "programming") {
+    return (
+      <div className='link-board'>
+        <ListOfLinks props={{ linkArray: ProgrammingLinks.daily }} />
+        <ListOfLinks props={{ linkArray: ProgrammingLinks.tools }} />
+        <ListOfLinks props={{ linkArray: ProgrammingLinks.docs }} />
+        <ListOfLinks props={{ linkArray: ProgrammingLinks.inspo }} />
+      </div>
+    )
+
+  }
+}
