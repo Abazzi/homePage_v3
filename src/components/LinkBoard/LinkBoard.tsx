144a4e575507d3a86b5a56eb32829c1ec73d7c38
Adam Bazzi <adam@adambazzi.ca>
2025-06-25 17:50:18 -0400

    feat: Finished LinkHeader code and styling
    LinkBoard now return a div called 'link-board-container' that holds the
    LinkBoard component and the LinkBoardHeader component

--- src/components/LinkBoard/LinkBoard.tsx
+++ src/components/LinkBoard/LinkBoard.tsx
@@ -2,29 +2,35 @@ import { DefaultLinkGroups, LinkBoardProps, ProgrammingLinkGroups } from "../../
 import ListOfLinks from "../ListOfLinks/ListOfLinks";
 import linkData from '../../assets/links.json';
 import './LinkBoard.css';
+import LinkBoardHeader from "../LinkBoardHeader/LinkBoardHeader";
 
 export default function LinkBoard({ props }: { props: LinkBoardProps }) {
   const ProgrammingLinks: ProgrammingLinkGroups = linkData.programming;
   const DefaultLinks: DefaultLinkGroups = linkData.default;
 
-  if (props.type == "default") {
+  if (props.linkMode == "default") {
     return (
-      <div className='link-board' data-theme={props.theme}>
-        <ListOfLinks props={{ linkArray: DefaultLinks.daily }} />
-        <ListOfLinks props={{ linkArray: DefaultLinks.social }} />
-        <ListOfLinks props={{ linkArray: DefaultLinks.media }} />
-        <ListOfLinks props={{ linkArray: DefaultLinks.server }} />
+      <div className="link-board-container">
+        <LinkBoardHeader props={{ linkMode: props.linkMode, setLinkMode: props.setLinkMode, theme: props.theme }} />
+        <div className='link-board' data-theme={props.theme}>
+          <ListOfLinks props={{ linkArray: DefaultLinks.daily, arrayName: 'daily' }} />
+          <ListOfLinks props={{ linkArray: DefaultLinks.social, arrayName: 'social' }} />
+          <ListOfLinks props={{ linkArray: DefaultLinks.media, arrayName: 'media' }} />
+          <ListOfLinks props={{ linkArray: DefaultLinks.server, arrayName: 'server' }} />
+        </div>
       </div>
     )
-  } else if (props.type == "programming") {
+  } else if (props.linkMode == "programming") {
     return (
-      <div className='link-board' data-theme={props.theme}>
-        <ListOfLinks props={{ linkArray: ProgrammingLinks.daily }} />
-        <ListOfLinks props={{ linkArray: ProgrammingLinks.tools }} />
-        <ListOfLinks props={{ linkArray: ProgrammingLinks.docs }} />
-        <ListOfLinks props={{ linkArray: ProgrammingLinks.inspo }} />
+      <div className="link-board-container">
+        <LinkBoardHeader props={{ linkMode: props.linkMode, setLinkMode: props.setLinkMode, theme: props.theme }} />
+        <div className='link-board' data-theme={props.theme}>
+          <ListOfLinks props={{ linkArray: ProgrammingLinks.daily, arrayName: 'daily' }} />
+          <ListOfLinks props={{ linkArray: ProgrammingLinks.tools, arrayName: 'tools' }} />
+          <ListOfLinks props={{ linkArray: ProgrammingLinks.docs, arrayName: 'docs' }} />
+          <ListOfLinks props={{ linkArray: ProgrammingLinks.inspo, arrayName: 'inspo' }} />
+        </div>
       </div>
     )
-
   }
 }
--- src/components/ListOfLinks/ListOfLinks.tsx
+++ src/components/ListOfLinks/ListOfLinks.tsx
@@ -5,6 +5,7 @@ import './ListOfLinks.css';
 export default function ListOfLinks({ props }: { props: ListOfLinksProps }) {
   return (
     <ul className='list-of-links'>
+      <li className='list-title'>{props.arrayName}</li>
       {props.linkArray.map((link) => {
         return <Link key={crypto.randomUUID()} props={{ name: link.name, url: link.url }} />
       })}
