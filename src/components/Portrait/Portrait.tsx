10286aabf4ee5c69dc4cdd22e52a8405ddfed83f
Adam Bazzi <adam@adambazzi.ca>
2026-03-14 14:39:49 -0400

    added tolarian academy theme wip

--- src/components/Portrait/Portrait.tsx
+++ src/components/Portrait/Portrait.tsx
@@ -1,27 +1,51 @@
 import { PortraitProps } from "../../types";
-import './Portrait.css';
-import alucardPortrait from '../../img/portraits/alucard_portrait.webp';
-import gaeasCradlePortrait from '../../img/portraits/gaeas_cradle.webp';
-import hhkbGuyPortrait from '../../img/portraits/hhkbGuy.webp';
-import solairePortrait from '../../img/portraits/solaire.gif';
+import "./Portrait.css";
+import alucardPortrait from "../../img/portraits/alucard_portrait.webp";
+import gaeasCradlePortrait from "../../img/portraits/gaeas_cradle.webp";
+import hhkbGuyPortrait from "../../img/portraits/hhkbGuy.webp";
+import solairePortrait from "../../img/portraits/solaire.gif";
+import tolarianPortrait from "../../img/portraits/tolarian.webp";
 
 export default function Portrait({ props }: { props: PortraitProps }) {
-
-  if (props.theme == 'alucard') {
+  if (props.theme == "alucard") {
     return (
-      <img src={alucardPortrait} className='portrait' data-theme={props.theme} />
-    )
-  } else if (props.theme == 'gaeas') {
+      <img
+        src={alucardPortrait}
+        className="portrait"
+        data-theme={props.theme}
+      />
+    );
+  } else if (props.theme == "gaeas") {
     return (
-      <img src={gaeasCradlePortrait} className='portrait' data-theme={props.theme} />
-    )
-  } else if (props.theme == 'hhkb') {
+      <img
+        src={gaeasCradlePortrait}
+        className="portrait"
+        data-theme={props.theme}
+      />
+    );
+  } else if (props.theme == "hhkb") {
     return (
-      <img src={hhkbGuyPortrait} className='portrait' data-theme={props.theme} />
-    )
+      <img
+        src={hhkbGuyPortrait}
+        className="portrait"
+        data-theme={props.theme}
+      />
+    );
+  } else if (props.theme == "tolarian") {
+    return (
+      <img
+        src={tolarianPortrait}
+        className="portrait"
+        data-theme={props.theme}
+      />
+    );
   } else {
     return (
-      <img src={solairePortrait} className='portrait' data-theme={props.theme} />
-    )
+      <img
+        src={solairePortrait}
+        className="portrait"
+        data-theme={props.theme}
+      />
+    );
   }
 }
